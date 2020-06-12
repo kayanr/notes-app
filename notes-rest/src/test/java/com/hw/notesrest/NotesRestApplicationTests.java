8870fdb18fe42a7f5aae7261a02477b2e4aede6a
Kayanr <kayan.reid@gmail.com>
2020-06-12 19:41:46 -0400

    Added notes-web and notes-rest folders and files.
    Added notes-web and notes-rest folders and files.

---
+++ notes-rest/src/test/java/com/hw/notesrest/NotesRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.notesrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
